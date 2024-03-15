ec2d910efdfc8afb7ffec5fd12c0a839d0efcc7f
ftholoza <francesco.tholozan@gmail.com>
2024-03-15 18:14:04 +0100

    dsf

--- read_file.c
+++ read_file.c
@@ -1,29 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub3d.h                                            :+:      :+:    :+:   */
+/*   read_file.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ftholoza <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/15 18:04:26 by ftholoza          #+#    #+#             */
-/*   Updated: 2024/03/15 18:13:16 by ftholoza         ###   ########.fr       */
+/*   Created: 2024/03/15 18:12:38 by ftholoza          #+#    #+#             */
+/*   Updated: 2024/03/15 18:13:08 by ftholoza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CUB3D_H
-# define CUB3D_H
-
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <fcntl.h>
-
-typedef struct info
-{
-	char	**textures;
-	char	**colors;
-	char	**maps;
-} info;
-
-
-#endif	
+# include "cub3dstruct.h"
