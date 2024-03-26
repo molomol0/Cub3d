48aa6656fba5a5b40df600885ce8df024376c8a5
ftholoza <francesco.tholozan@gmail.com>
2024-03-26 19:35:29 +0100

    point in minimap

--- src/minimap.c
+++ src/minimap.c
@@ -6,12 +6,18 @@
 /*   By: francesco <francesco@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 08:59:46 by francesco         #+#    #+#             */
-/*   Updated: 2024/03/26 19:08:13 by francesco        ###   ########.fr       */
+/*   Updated: 2024/03/26 19:35:13 by francesco        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "cub_struct.h"
 # include "cub.h"
+
+int	create_trgb(int t, int r, int g, int b)
+{
+	return (t << 24 | r << 16 | g << 8 | b);
+}
+
 
 int draw_minimap(t_cub *cub)
 {
@@ -26,31 +32,26 @@ int draw_minimap(t_cub *cub)
     y = 0;
     x_max = (int)cub->player->map_x - 20;
     y_max = (int)cub->player->map_y - 20;
-    //printf("%f, %f\n", x_max, y_max);
-    //printf("here: %d, %d\n", (int)cub->player->map_x, (int)cub->player->map_y);
     scale = 40 / (double)cub->minimap->width;
-    //printf("%d\n, %d\n", cub->minimap->width, cub->minimap->height);
-    //printf("here: %p\n", cub->minimap->addr);
     while (y < cub->minimap->height - 1)
     {
         while (x < cub->minimap->width - 1)
         {
             if (check_if_null(cub->map, x_max + x * scale, y_max + y * scale))
             {
-                put_pixel(cub->minimap, x, y, convert_color(cub->floor));
-               // flag = 1;
+                put_pixel(cub->minimap, x, y, create_trgb(0, 0, 0, 0));
             }
             else
             {
-                put_pixel(cub->minimap, x, y, convert_color(cub->ceiling));
+                put_pixel(cub->minimap, x, y, create_trgb(0, 255, 255, 255));
             }
+            //printf("%f, %f\n", roundf(x_max + x * scale));
+            if ((roundf(x_max + x * scale) == cub->player->ray->map_x) && (roundf(y_max + y * scale) == cub->player->ray->map_y))
+                put_pixel(cub->minimap, x, y, create_trgb(0, 205, 92, 92));
             x ++;
         }
-        //printf("%f: ", x_max + x * scale);
-        //x_max = (int)cub->player->map_x - 15;
         x = 0;
         y++;
     }
-    //printf("y_max: %f: ", y_max + y * scale);
     return (0);
 }
