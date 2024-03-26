f2fc744abd2521afa37cd3da313dd3ccf670f639
ftholoza <francesco.tholozan@gmail.com>
2024-03-26 19:08:33 +0100

    minimap

--- src/minimap.c
+++ src/minimap.c
@@ -6,7 +6,7 @@
 /*   By: francesco <francesco@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 08:59:46 by francesco         #+#    #+#             */
-/*   Updated: 2024/03/26 18:53:24 by francesco        ###   ########.fr       */
+/*   Updated: 2024/03/26 19:08:13 by francesco        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,18 @@ int draw_minimap(t_cub *cub)
 
     x = 0;
     y = 0;
-    x_max = (int)cub->player->map_x - 15;
-    y_max = (int)cub->player->map_y - 15;
-    printf("%f, %f\n", x_max, y_max);
-    printf("%d, %d\n", (int)cub->player->map_x, (int)cub->player->map_y);
-    scale = 30 / (double)cub->minimap->width;
+    x_max = (int)cub->player->map_x - 20;
+    y_max = (int)cub->player->map_y - 20;
+    //printf("%f, %f\n", x_max, y_max);
+    //printf("here: %d, %d\n", (int)cub->player->map_x, (int)cub->player->map_y);
+    scale = 40 / (double)cub->minimap->width;
     //printf("%d\n, %d\n", cub->minimap->width, cub->minimap->height);
     //printf("here: %p\n", cub->minimap->addr);
     while (y < cub->minimap->height - 1)
     {
         while (x < cub->minimap->width - 1)
         {
-            if (check_if_null(cub->map, y_max + y * scale, x_max + x * scale))
+            if (check_if_null(cub->map, x_max + x * scale, y_max + y * scale))
             {
                 put_pixel(cub->minimap, x, y, convert_color(cub->floor));
                // flag = 1;
@@ -46,10 +46,11 @@ int draw_minimap(t_cub *cub)
             }
             x ++;
         }
-        //printf("%f\n", x_max + x * scale);
+        //printf("%f: ", x_max + x * scale);
         //x_max = (int)cub->player->map_x - 15;
         x = 0;
         y++;
     }
+    //printf("y_max: %f: ", y_max + y * scale);
     return (0);
 }
